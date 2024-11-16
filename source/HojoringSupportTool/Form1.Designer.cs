e9c4d335d21af38fd8481f15974e8469f891b890
bonbon <yosiox@gmail.com>
2024-11-16 19:44:18 +0900

    ver 0.01

---
+++ source/HojoringSupportTool/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace HojoringSupportTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            GameDataPath = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            radioButton6 = new RadioButton();
+            groupBox1 = new GroupBox();
+            radioButton7 = new RadioButton();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // GameDataPath
+            // 
+            GameDataPath.Location = new Point(121, 20);
+            GameDataPath.Name = "GameDataPath";
+            GameDataPath.Size = new Size(506, 23);
+            GameDataPath.TabIndex = 0;
+            GameDataPath.Text = "C:\\Program Files (x86)\\SquareEnix\\FINAL FANTASY XIV - A Realm Reborn\\game\\sqpack";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 15);
+            label1.TabIndex = 1;
+            label1.Text = "GameDataPath";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(514, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Generate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Checked = true;
+            radioButton1.Location = new Point(32, 26);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(38, 19);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "en";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(76, 26);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(34, 19);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "ja";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(116, 26);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(38, 19);
+            radioButton3.TabIndex = 5;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "de";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(160, 26);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(33, 19);
+            radioButton4.TabIndex = 6;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "fr";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(199, 26);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(35, 19);
+            radioButton5.TabIndex = 7;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "kr";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(240, 26);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(48, 19);
+            radioButton6.TabIndex = 8;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "cn-s";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton7);
+            groupBox1.Controls.Add(radioButton6);
+            groupBox1.Controls.Add(radioButton5);
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(121, 49);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(362, 66);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Language";
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(294, 26);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(47, 19);
+            radioButton7.TabIndex = 9;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "cn-t";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(771, 132);
+            Controls.Add(groupBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(GameDataPath);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            SizeGripStyle = SizeGripStyle.Hide;
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox GameDataPath;
+        private Label label1;
+        private Button button1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private RadioButton radioButton6;
+        private GroupBox groupBox1;
+        private RadioButton radioButton7;
+    }
+}
---
+++ source/HojoringSupportTool/Form1.cs
@@ -0,0 +1,118 @@
+using Lumina;
+using Lumina.Data;
+using Lumina.Excel;
+using Lumina.Excel.Sheets;
+using Lumina.Text.ReadOnly;
+using System.Diagnostics;
+
+namespace HojoringSupportTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            try
+            {
+                var ext = "";
+                LuminaOptions options = new LuminaOptions();
+                if (radioButton1.Checked)
+                {
+                    options.DefaultExcelLanguage = Language.English;
+                    ext = ".en-US.csv";
+                }
+                if (radioButton2.Checked)
+                {
+                    options.DefaultExcelLanguage = Language.Japanese;
+                    ext = ".ja-JP.csv";
+                }
+                if (radioButton3.Checked)
+                {
+                    options.DefaultExcelLanguage = Language.German;
+                    ext = ".de-DE.csv";
+                }
+                if (radioButton4.Checked)
+                {
+                    options.DefaultExcelLanguage = Language.French;
+                    ext = ".fr-FR.csv";
+                }
+                if (radioButton5.Checked)
+                {
+                    options.DefaultExcelLanguage = Language.Korean;
+                    ext = ".ko-KR.csv";
+                }
+                if (radioButton6.Checked)
+                {
+                    options.DefaultExcelLanguage = Language.ChineseSimplified;
+                    ext = ".zh-CN.csv";
+                }
+                if (radioButton7.Checked)
+                {
+                    options.DefaultExcelLanguage = Language.ChineseTraditional;
+                    ext = ".zh-CN.csv";
+                }
+                options.LoadMultithreaded = true;
+
+                var lumina = new Lumina.GameData(GameDataPath.Text, options);
+
+
+                string text = "";
+
+                // Action
+                var sheet_a = lumina.GetExcelSheet<Lumina.Excel.Sheets.Action>();
+                if (sheet_a != null)
+                {
+                    text = "key,0,1\n#,Name,AttackType\nint32,str,int32\n";
+                    foreach (var item in sheet_a)
+                    {
+                        text += item.RowId + ",\"" + item.Name.ToString() + "\"," + item.AttackType.RowId + "\n";
+                    }
+                    File.WriteAllText(@"Action" + ext, text, System.Text.Encoding.UTF8);
+                }
+
+                // Status
+                var sheet_s = lumina.GetExcelSheet<Lumina.Excel.Sheets.Status>();
+                if (sheet_s != null)
+                {
+                    text = "key,0\n#,Name\nint32,str\n";
+                    foreach (var item in sheet_s)
+                    {
+                        text += item.RowId + ",\"" + item.Name.ToString() + "\"\n";
+                    }
+                    File.WriteAllText(@"Status" + ext, text, System.Text.Encoding.UTF8);
+                }
+
+                var sheet_t = lumina.GetExcelSheet<Lumina.Excel.Sheets.TerritoryType>();
+                // TerritoryType
+                if (sheet_t != null)
+                {
+                    text = "key,0,1,2\n#,TerritoryIntendedUse,Resident,ContentFinderCondition\nint32,TerritoryIntendedUse,uint16,ContentFinderCondition\n";
+                    foreach (var item in sheet_t)
+                    {
+                        ContentFinderCondition cfc = item.ContentFinderCondition.Value;
+                        RowRef<TerritoryType> territory = cfc.TerritoryType;
+                        RowRef<TerritoryIntendedUse> intendedUse = item.TerritoryIntendedUse;
+                        
+                        if (territory.IsValid && !cfc.Name.IsEmpty)
+                        {
+                            text += item.RowId + ",\"" + intendedUse.RowId + "\",\"" + item.Resident + "\",\"" + cfc.Name.ToString() + "\"\n";
+                        }
+                    }
+                    File.WriteAllText(@"TerritoryType" + ext, text, System.Text.Encoding.UTF8);
+                }
+
+                MessageBox.Show("finished.");
+            } catch
+            {
+
+            }
+
+            button1.Enabled = true;
+        }
+    }
+}
